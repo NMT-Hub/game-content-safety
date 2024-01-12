32b0c562191f5c6bfabb7de3cbcb80dc387b7182
BrightXiaoHan <beatmight@gmail.com>
2024-01-12 10:43:11 +0800

    准备了训练xlm-roberta数据

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -1,6 +1,8 @@
 import fileinput
+import os
 import logging
-import re
+import random
+import subprocess
 
 avaliable_labels = [
     "compliant",
@@ -17,6 +19,8 @@ avaliable_labels = [
     "diverting players to other games",
 ]
 
+label_2_text = {key: [] for key in avaliable_labels}
+
 
 for line in fileinput.input("./datasets/all.tsv"):
     line = line.strip()
@@ -28,8 +32,35 @@ for line in fileinput.input("./datasets/all.tsv"):
 
     for label in avaliable_labels:
         if label in explain:
-            print("\t".join([label, content]))
+            label_2_text[label].append(content)
 
-with open("./datasets/labels.txt", "w") as f:
-    for label in avaliable_labels:
-        f.write(label + "\n")
+# print number of samples for each label
+for label in avaliable_labels:
+    logging.warning("Label: %s, number of samples: %d", label, len(label_2_text[label]))
+
+# remove old data
+os.remove("./datasets/dev.tsv")
+os.remove("./datasets/test.tsv")
+os.remove("./datasets/train.tsv")
+
+# shuffle and extract 10 sample each label as dev and test set respectively.
+# remaining as training set.
+
+for label in avaliable_labels:
+    random.shuffle(label_2_text[label])
+    with open("./datasets/dev.tsv".format(label), "a") as f:
+        for line in label_2_text[label][:10]:
+            f.write(line + "\t" + label + "\n")
+    with open("./datasets/test.tsv".format(label), "a") as f:
+        for line in label_2_text[label][10:20]:
+            f.write(line + "\t" + label + "\n")
+    with open("./datasets/train.tsv".format(label), "a") as f:
+        for line in label_2_text[label][20:]:
+            f.write(line + "\t" + label + "\n")
+
+
+# shuffle training set
+subprocess.call(
+    "shuf ./datasets/train.tsv > ./datasets/train.tsv.tmp && mv ./datasets/train.tsv.tmp ./datasets/train.tsv",
+    shell=True,
+)
