f4f7bb8782226a9f7ce69532f9ce491afbd83006
BrightXiaoHan <beatmight@gmail.com>
2024-01-31 10:31:42 +0800

    修复了google翻译脚本

--- google_translate.py
+++ google_translate.py
@@ -1,9 +1,11 @@
 import asyncio
 import functools
+import logging
 import os
 import re
 import sys
 from typing import List
+from tqdm import tqdm
 
 import httpx
 import langcodes
@@ -11,16 +13,22 @@ from more_itertools import chunked
 
 GOOGLE_API_KEY = "AIzaSyDMx3Lf9EGMyve9EArG0IJ__FkJYJ5MPBQ"
 URL = f"https://translation.googleapis.com/language/translate/v2?key={GOOGLE_API_KEY}"
+GOOGLE_SUPPORTED_LANGUAGES = None
 
 
 @functools.cache
 def get_supported_languages() -> List[str]:
-    # Get the supported languages from Google
-    url = "https://translation.googleapis.com/language/translate/v2/languages"
-    params = {"key": GOOGLE_API_KEY}
-    response = httpx.get(url, params=params)
-    data = response.json()
-    return [d["language"] for d in data["data"]["languages"]]
+    global GOOGLE_SUPPORTED_LANGUAGES
+    if GOOGLE_SUPPORTED_LANGUAGES:
+        return GOOGLE_SUPPORTED_LANGUAGES
+    else:
+        # Get the supported languages from Google
+        url = "https://translation.googleapis.com/language/translate/v2/languages"
+        params = {"key": GOOGLE_API_KEY}
+        response = httpx.get(url, params=params)
+        data = response.json()
+        GOOGLE_SUPPORTED_LANGUAGES = [d["language"] for d in data["data"]["languages"]]
+        return GOOGLE_SUPPORTED_LANGUAGES
 
 
 async def batch_translate_texts(
@@ -36,7 +44,10 @@ async def batch_translate_texts(
                 f"Language code {target_language_code} is not supported by Google Translate."
             )
 
-    if source_language_code not in supported_languages:
+    if (
+        source_language_code not in supported_languages
+        and source_language_code != "auto"
+    ):
         source_language_code = langcodes.closest_supported_match(
             source_language_code, supported_languages
         )
@@ -52,11 +63,17 @@ async def batch_translate_texts(
     for chunk in chunked(
         texts, 128
     ):  # Google Translate API has a limit of 128 texts per request
-        payload = {
-            "q": chunk,
-            "target": target_language_code,
-            "source": source_language_code,
-        }
+        if source_language_code == "auto":
+            payload = {
+                "q": chunk,
+                "target": target_language_code,
+            }
+        else:
+            payload = {
+                "source": source_language_code,
+                "q": chunk,
+                "target": target_language_code,
+            }
         async with httpx.AsyncClient() as client:
             response = await client.post(URL, json=payload)
             data = response.json()
@@ -88,11 +105,10 @@ if __name__ == "__main__":
     source_language_code = sys.argv[1]
     target_language_code = sys.argv[2]
 
-    for batch in chunked(sys.stdin, 20):
+    for batch in tqdm(chunked(sys.stdin, 100)):
         batch = [line.strip() for line in batch]
         results = asyncio.run(
             batch_translate_texts(batch, source_language_code, target_language_code)
         )
         for result in results:
             print(result)
-
